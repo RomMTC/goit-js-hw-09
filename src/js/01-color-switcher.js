191da87b84a952ae2fe9066c19a50496343617c5
Roman Lizunov <rlizunoff@gmail.com>
2022-08-14 19:34:18 +0300

    01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,18 @@
 import Notiflix from 'notiflix';
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+refs.startBtn.addEventListener('click', changeBodyColor);
+// refs.stopBtn.addEventListener('click',);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeBodyColor(color) {
+  setInterval(getRandomHexColor, 1000);
+}
+// setInterval(getRandomHexColor(), 1000)
+// function start
